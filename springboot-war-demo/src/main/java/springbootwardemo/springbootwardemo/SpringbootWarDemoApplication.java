649386c5203ab6339397a045bbaa25389c038c1f
MathireddiJayasree <mathireddi.jayasree@capgemini.com>
2024-02-05 23:02:44 +0530

    initial commit

---
+++ springboot-war-demo/src/main/java/springbootwardemo/springbootwardemo/SpringbootWarDemoApplication.java
@@ -0,0 +1,13 @@
+package springbootwardemo.springbootwardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWarDemoApplication.class, args);
+	}
+
+}
